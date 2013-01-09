d16baa2fc17cde79401c0e26706bca6ed0e5698b
Julian C. Dunn <jdunn@secondmarket.com>
2013-01-09 10:49:10 -0500

    SOP-2659 Fix broken syntax after unit testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,34 +10,34 @@
 require 'chef/util/file_edit'
 
 case node['platform_family']
-  when "debian"
-    # Make sure it's installed. It would be a pretty broken system
-    # that didn't have it.
-    package "tzdata"
+when "debian"
+  # Make sure it's installed. It would be a pretty broken system
+  # that didn't have it.
+  package "tzdata"
 
-    template "/etc/timezone" do
-      source "timezone.conf.erb"
-      owner 'root'
-      group 'root'
-      mode 0644
-      notifies :run, 'bash[dpkg-reconfigure tzdata]'
-    end
-
-    bash 'dpkg-reconfigure tzdata' do
-      user 'root'
-      code "/usr/sbin/dpkg-reconfigure -f noninteractive tzdata"
-      action :nothing
-    end
-
-  when "fedora", "rhel"
-    package "tzdata"
-
-    link "/etc/localtime" do
-      to "/usr/share/zoneinfo/#{node['tz']}"
-    end
-
-    clock = Chef::Util::FileEdit.new("/etc/sysconfig/clock")
-    clock.search_file_replace_line(/^ZONE=.*$/, "ZONE=\"#{node['tz']}\"")
-    clock.write_file
+  template "/etc/timezone" do
+    source "timezone.conf.erb"
+    owner 'root'
+    group 'root'
+    mode 0644
+    notifies :run, 'bash[dpkg-reconfigure tzdata]'
   end
+
+  bash 'dpkg-reconfigure tzdata' do
+    user 'root'
+    code "/usr/sbin/dpkg-reconfigure -f noninteractive tzdata"
+    action :nothing
+  end
+
+when "fedora", "rhel"
+  package "tzdata"
+
+  link "/etc/localtime" do
+    to "/usr/share/zoneinfo/#{node['tz']}"
+  end
+
+  clock = Chef::Util::FileEdit.new("/etc/sysconfig/clock")
+  clock.search_file_replace_line(/^ZONE=.*$/, "ZONE=\"#{node['tz']}\"")
+  clock.write_file
+
 end
